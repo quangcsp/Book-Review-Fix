13310f8cbfbfbbd18c74dda01a3467fa6a7e8610
shadowmonkey95 <vuphamas@gmail.com>
2017-10-09 00:55:08 +0700

    Add default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/avatar-missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: ':placeholder'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
