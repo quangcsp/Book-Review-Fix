54ef725a0e8ccec044dc7d9f23da6203c50c0d0b
Duy Nguyen <duyso1000@gmail.com>
2017-10-30 10:39:00 +0700

    Fix interface

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def get_user_name(id)
+    @curr = User.find(id)
+    if @curr.name.nil?
+      @name = @curr.email
+    else
+      @name = @curr.name
+    end
+    return @name
+  end
 end
